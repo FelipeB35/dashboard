b472f9a2bc94c7707def96e8e75bbbbd70a6c712
Felipe B <20200754@esen.edu.sv>
2022-04-12 19:13:09 -0600

    login y register set

--- routes/auth.py
+++ routes/auth.py
@@ -24,7 +24,7 @@ def login():
         if currentUser:
             if bcrypt.check_password_hash(currentUser.password, password):
                 login_user(currentUser)
-                return redirect(url_for("orders.create"))
+                return redirect(url_for("dashboard.home"))
     return render_template("login.html", form=form)
     return redirect('create.html')
 
