dc76e375d6859fc48614b210bb340ba85d841add
Gustavo <naves.gustavo8@gmail.com>
2022-04-16 18:43:56 -0600

    discrepancia arreglada

--- routes/post.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, redirect, url_for
-from utils.db import db
-
-post = Blueprint("post", __name__)
-
-
-@post.route("/post")
-def home():
-    return render_template("ejemplo1.html")
