d2ea8ed5351be5be4053db89262024b1d2d4800d
Felipe B <20200754@esen.edu.sv>
2022-04-19 11:10:08 -0600

    Preparar repo

--- routes/investigacion.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, redirect, url_for
-from utils.db import db
-
-inv = Blueprint("investigacion", __name__)
-
-
-@inv.route("/investigacion")
-def investigacion():
-    return render_template("investigacion.html")
--- routes/nosotros.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template, redirect, url_for
-from utils.db import db
-
-about = Blueprint("nosotros", __name__)
-
-
-@about.route("/nosotros")
-def nosotros():
-    return render_template("nosotros.html")
