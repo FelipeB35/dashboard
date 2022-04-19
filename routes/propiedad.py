6298e93a3d71d0822f500ab9e79b768b949fb257
DECS9 <cevallosdavid9@gmail.com>
2022-04-19 10:23:24 -0600

    Fix it

--- routes/propiedad.py
+++ routes/propiedad.py
@@ -5,7 +5,6 @@ from models.property import Property
 prop = Blueprint("propiedad", __name__)
 
 
-@prop.route("/propiedad/<int:id>")
-def propiedad(id):
-    propertyList = Property.query.filter_by(id=id).first()
-    return render_template("propiedad.html", propiedad = propertyList)
+@prop.route("/propiedad/")
+def propiedad():
+    return render_template("propiedad.html")
